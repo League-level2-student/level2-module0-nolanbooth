8fc322503e7a5492876e66fe2c2d774350b8784a
nolanbooth <56123783+nolanbooth@users.noreply.github.com>
2021-09-24 16:53:12 -0700

    Update _00_ArrayCheatSheet.java

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,33 +1,30 @@
 package arrays;
 
-public class _00_ArrayCheatSheet {
+import java.util.Arrays;
+import java.util.Random;
+
+public class _00_ArrayCheatSheet_DONE {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		String[] random = {"Dog","Cat","Mouse","Bat","Bird"};
-		//2. print the third element in the array
+		int randomNum;
+		Random rand = new Random();
+		String[] random = { "Dog", "Cat", "Mouse", "Bat", "Bird" };
 		System.out.println(random[2]);
-		//3. set the third element to a different value
 		random[2] = "Rat";
-		//4. print the third element again
 		System.out.println(random[2]);
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		for(int i = 0; i < random.length; i++) {
+		for (int i = 0; i < random.length; i++) {
 			System.out.println(random[i]);
-			
 		}
-		
-		//6. make an array of 50 integers
-		int[] onetwofifty = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
-		//7. use a for loop to make every value of the integer array a random number
-		for(int i = 0; i < onetwofifty.length; i++) {
-			
-			
+		int[] oneTwoFifty = new int[50];
+		for (int i = 0; i < oneTwoFifty.length; i++) {
+			oneTwoFifty[i] = rand.nextInt(21);
 		}
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		System.out.println(Arrays.toString(oneTwoFifty));
+		int largest = oneTwoFifty[0];
+		for (int i = 1; i < oneTwoFifty.length; i++) {
+			if (largest <= oneTwoFifty[i]) {
+				largest = oneTwoFifty[i];
+			}
+		}
+		System.out.println(largest);
 	}
 }
